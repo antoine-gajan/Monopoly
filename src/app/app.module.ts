fb05cc8c74bea5da8b1c4e7bc2b37ca6eedf966a
Antoine <antoine1.gajan@gmail.com>
2023-02-22 14:43:30 +0100

    Add registration and login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule
