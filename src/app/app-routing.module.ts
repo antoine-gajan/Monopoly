3738d2fc1f5c951447960dbb0dbe3cad1db8e4bd
pifica2002 <pifica2002@gmail.com>
2023-02-28 15:13:07 +0100

    Web con primera pantalla inicial compelta 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegistrationComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
