c938a4ede7f3203960797b03076491deddf1fe9c
pifica2002 <pifica2002@gmail.com>
2023-03-15 11:28:17 +0100

    Cambios en la web generales, falta de probar que conecta con la base de datos y seguir con el tablero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import {RegistrationComponent} from "./registration/registration.component";
-import {BoardComponent} from "./board/board.component";
+import { RegistrationComponent} from "./registration/registration.component";
+import { BoardComponent} from "./board/board.component";
+import { PantallaComponent } from './pantalla/pantalla.component';
+import { ReglasComponent } from './reglas/reglas.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: RegistrationComponent },
-  { path: 'board', component: BoardComponent }
+  { path: 'board', component: BoardComponent },
+  { path: 'pantalla', component: PantallaComponent },
+  { path: 'reglas', component: ReglasComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'contacto', component: ContactoComponent }
 ];
 
 @NgModule({
