ea62d836c501d59524a70ddb5a4129359b79ef99
pifica2002 <pifica2002@gmail.com>
2023-04-01 11:01:29 +0200

    ultimos cambios web

--- src/app/pantalla/pantalla.component.ts
+++ src/app/pantalla/pantalla.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -11,8 +12,38 @@ import {Location} from "@angular/common";
 export class PantallaComponent {
 
   constructor(
+    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private location: Location
   ) {}
+
+  form!: FormGroup; // Store the form
+  loading = false; // If form send but not done
+  submitted = false; // If form is sent
+  nombre_jugador: string | undefined;
+  /*form!: FormGroup; // Store the form
+  loading = false; // If form send but not done
+  submitted = false; // If form is sent
+  username: string | undefined;
+  password: string | undefined;
+  
+
+  ngOnInit() {
+    /// Init the form with validators
+    this.form = new FormGroup({
+    username: new FormControl(this.username, [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.email
+    ]),
+      password: new FormControl(this.password, [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  });
+  }
+
+  onSubmit(){
+  }*/
 
 }
