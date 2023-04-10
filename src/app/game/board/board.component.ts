f0cab5ac09c8a6b4430e1b776642c25340c98321
Antoine <antoine1.gajan@gmail.com>
2023-04-10 11:05:31 +0200

    Add dices image when rolling dices

--- src/app/game/board/board.component.ts
+++ src/app/game/board/board.component.ts
@@ -17,4 +17,42 @@ export class BoardComponent {
     // Display for degugging
     console.table(this.dices)
   }
+
+  get_image_from_dice_value(value: Number): String{
+    // Function to get the image path from the dice value
+    return "../../../assets/images/dice/" + value + ".png";
+  }
+
+  convert_position_to_number(x: Number, y: Number): Number{
+    // Function to convert position (x, y) to number between 1 and 39
+    if (x == 0){
+      return +20 + +y;
+    }
+    else if (x == 10){
+      return +10 - +y;
+    }
+    else if (y == 0){
+      return 20 - +x;
+    }
+    else if (y == 10){
+      return +30 + +x;
+    }
+    else{
+      console.log("Error: x and y are not valid");
+      return 0;
+    }
+  }
+
+  display_position(x: Number, y: Number): void{
+    // Function to display position (x, y) in the board
+    let position = this.convert_position_to_number(x, y);
+    // Get the property of the element with id = position
+    let position_element = document.getElementById(position.toString());
+    if (position_element != null){
+      /// TODO : Add player's image in the center of the property
+    }
+    else{
+      console.log("Error: position is not valid");
+    }
+  }
 }
