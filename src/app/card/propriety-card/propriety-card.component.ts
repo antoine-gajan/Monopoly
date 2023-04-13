192f5b195e0a814ab6a7ad2e84504f143cd5bafe
Antoine <antoine1.gajan@gmail.com>
2023-04-13 14:30:43 +0200

    Add propriety class to link with database

--- src/app/card/propriety-card/propriety-card.component.ts
+++ src/app/card/propriety-card/propriety-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Propriety} from "../propriety";
 
 @Component({
   selector: 'app-propriety-card',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ["../../../../node_modules/bootstrap/dist/css/bootstrap.min.css", './propriety-card.component.css']
 })
 export class ProprietyCardComponent {
+  @Input() h : Number;
+  @Input() v : Number;
+
+  propriety : Propriety;
   constructor() {}
+
+  get_propriety(){
+    /// TODO : Get the property from the server
+    if (this.h == 5 || this.v == 5){
+      /// Get "festividad"
+    }
+    else if ((this.h == 0 && this.v == 8) || (this.h == 8 && this.v == 0)){
+      /// Get "impuesto"
+    }
+    else if ((this.h == 10 && this.v == 6) || (this.h == 8 && this.v == 10) || (this.h == 10 && this.v == 0) || (this.h == 0 || this.v == 0)){
+      /// Get "tipo special"
+    }
+  }
 }
---
+++ src/app/card/propriety.ts
@@ -0,0 +1,16 @@
+export class Propriety {
+  nombre: string;
+  precioCompra: number;
+  matricula: number;
+  precio1C: number;
+  precio2C: number;
+  precio3C: number;
+  precio4C: number;
+  devolucionMatricula: number;
+  tipo: string;
+  coordenadas: {
+    h: number;
+    v: number;
+  };
+  cuatrisemestre: number;
+}
