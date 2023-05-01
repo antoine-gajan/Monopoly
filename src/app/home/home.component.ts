381a9e1ab1e0281caeb60b828662a724e133904b
pifica2002 <819658@unizar.es>
2023-05-01 23:12:41 +0200

    Cambios generales para mejor funcionameinto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  // función que permite volver arriba en la página
+  volverArriba() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -13,4 +13,24 @@ export class NosotrosComponent {
     private route: ActivatedRoute,
     private location: Location
   ) {}
+  // función que permite volver arriba en la página
+  volverArriba() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  ngOnInit(): void {
+    setTimeout(function() {
+      const floatingImage = document.getElementById('floating-image');
+      const mainContainer = document.getElementById('main');
+      if (floatingImage && mainContainer) {
+        floatingImage.style.opacity = '1';
+        floatingImage.style.transform = 'translateY(0)';
+        mainContainer.style.opacity = '1';
+        mainContainer.style.transform = 'translateY(0)';
+      }
+    }, 10);
+  }
+  
+  
+  
+
 }
