d57fb0284b6f2f60fc8f10886dfe9ffc569e1d99
pifica2002 <819658@unizar.es>
2023-05-08 01:08:18 +0200

    REvisar esperar sala component que funciona

--- src/app/esperar_sala/esperar_sala.component.ts
+++ src/app/esperar_sala/esperar_sala.component.ts
@@ -52,31 +52,6 @@ export class EsperarSalaComponent implements OnInit{
     .catch((error) => {
       console.error('Error al obtener usuarios conectados:', error);
     });
-    
-    /*
-    
-    if(this.veces==0){
-      this.mostrarListaJugadores = true;
-      console.log("esperar sala: ", this.game_id);
-      const datos = { idPartida: this.game_id };
-      this.userService.getNumJugadores(datos).subscribe((numJugadores) => {
-        this.maxPlayers = numJugadores;
-        console.log("maxPlayers: ", this.maxPlayers);
-      });
-      this.veces = 1;
-    }
-
-    if (this.nb_players_connected === this.maxPlayers) {
-      if (this.username === this.player_creator_of_game) {
-          // Get button with id = "start" from html and activate it
-          this.mostrarBotonEmpezar = true;
-          this.mostrarBotonUnirse = false;
-      } else {
-          this.mostrarBotonEmpezar = false;
-          this.mostrarBotonUnirse = true;
-        }
-    }
-    */
    
   }
 
@@ -95,27 +70,7 @@ export class EsperarSalaComponent implements OnInit{
     .catch((error) => {
       console.error('Error al obtener usuarios conectados:', error);
     });
-    /*
-    this.gameService.get_list_players(this.game_id).subscribe((response: PlayerListResponse) => {
-        // Get list of players
-        this.list_players = response.listaJugadores;
-        // Get number of players connected
-        this.nb_players_connected = this.list_players.length;
-        // Get creator of game
-        this.player_creator_of_game = this.list_players[0];
-        console.log("ACTUALIZA INFO: ", this.nb_players_connected, this.maxPlayers);
-        if (this.nb_players_connected === this.maxPlayers) {
-          if (this.username === this.player_creator_of_game) {
-              this.mostrarBotonEmpezar = true;
-              this.mostrarBotonUnirse = false;
-
-          } else {
-              this.mostrarBotonEmpezar = false;
-              this.mostrarBotonUnirse = true;
-            }
-        }
-    });
-    */
+    
 }
 
   // Función para acceder al tablero de juego para el creador de la partida
@@ -144,20 +99,5 @@ export class EsperarSalaComponent implements OnInit{
     }, 2500);
     this.actualize_game_info();
     this.mostrarListaJugadores = true;
-    /*this.gameService.get_list_players(this.game_id).subscribe((response: PlayerListResponse) => {
-      this.list_players = response.listaJugadores;          // Se obtiene la lista de jugadores
-      this.nb_players_connected = this.list_players.length; // Se obtiene el numero de jugadores conectado
-      this.player_creator_of_game = this.list_players[0];   // Se obtiene el nombre del usuario del jugadore creador de la partida
-      console.log("REFRESH: ", this.nb_players_connected, this.maxPlayers);
-      if (this.nb_players_connected === this.maxPlayers) {
-        if (this.username === this.player_creator_of_game) {
-            this.mostrarBotonEmpezar = true;
-            this.mostrarBotonUnirse = false;
-        } else {
-            this.mostrarBotonEmpezar = false;
-            this.mostrarBotonUnirse = true;
-          }
-      }
-    });*/
   }
 }
