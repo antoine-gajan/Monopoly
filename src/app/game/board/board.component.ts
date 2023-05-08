639f1c3b413f99b2472015aab4bfe7b10b69f41b
pifica2002 <819658@unizar.es>
2023-05-08 19:21:44 +0200

    Versión antes de empezar a tocar la conexión al socket con Tablero y todo lo demás

--- src/app/game/board/board.component.ts
+++ src/app/game/board/board.component.ts
@@ -51,7 +51,7 @@ export class BoardComponent implements OnInit, OnDestroy {
     "../../../assets/images/dice/4.png",
     "../../../assets/images/dice/5.png",
     "../../../assets/images/dice/6.png"
-  ];
+  ]; 
 
   // Variables for the game functionning
   message: string;
